1a68231d93c1d2e25cf28497ee8dfeb789ed3b32
Rif7 <rifraf92@gmail.com>
2018-01-14 14:13:37 +0100

    Added whipped cream checkbox and functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,7 +10,9 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -20,21 +22,32 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
     int quantity;
     static final double oneCupPrice = 2.55;
+    static final double whippedCreamPrice = 0.45;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         quantity = 0;
-        display(quantity);
-        displayPrice(quantity *oneCupPrice);
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method returns calculated Price
+     */
+    public double calculatePrice() {
+        double oneCubTotalPrice = oneCupPrice;
+        if (hasWhippedCream()) {
+            oneCubTotalPrice += whippedCreamPrice;
+        }
+        return quantity * oneCubTotalPrice ;
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity *oneCupPrice);
+        displayMessage(createOrderSummary(calculatePrice()));
     }
 
     /**
@@ -42,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -51,23 +64,46 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         if (quantity > 0) {
             quantity--;
-            display(quantity);
+            displayQuantity(quantity);
         }
+    }
+
+    private boolean hasWhippedCream() {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.cream_check_box);
+        return whippedCreamCheckBox.isChecked();
+    }
+
+    private String getAnswer(boolean statement) {
+        return statement ? "Yes" : "No";
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(double price) {
-        TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
-        String priceText = "Total:  " + NumberFormat.getCurrencyInstance().format(price);
+    /**
+     * Prepare summary
+     * @param orderPrice price of order
+     * @return text summary
+     */
+    private String createOrderSummary(double orderPrice) {
+        Log.v("MAinActivity", "Has whipped cream? " + getAnswer(hasWhippedCream()));
+        String priceText = "Name: Kapitan Kunal";
+        priceText += "\nAdd whipped cream? " + getAnswer(hasWhippedCream());
+        priceText += "\nQuantity: " + quantity;
+        priceText += "\nTotal: " + NumberFormat.getCurrencyInstance().format(orderPrice);
         priceText += "\nThank You!";
-        quantityTextView.setText(priceText);
+        return priceText;
     }
+
+    private void displayMessage(String message) {
+        TextView quantityTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        quantityTextView.setText(message);
+    }
+
 
 }
